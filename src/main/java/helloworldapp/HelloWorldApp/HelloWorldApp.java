873444b4bc6a0fa60d61ebbd1940e510bfa8c122
LinKaTITo <diegocaruso@gmail.com>
2019-09-23 22:24:41 -0300

    Error Arreglado

--- src/main/java/helloworldapp/HelloWorldApp/HelloWorldApp.java
+++ src/main/java/helloworldapp/HelloWorldApp/HelloWorldApp.java
@@ -14,7 +14,7 @@ public class HelloWorldApp {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {;
+    public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World Fantastic 17 30!"); // Display the string.
     }
